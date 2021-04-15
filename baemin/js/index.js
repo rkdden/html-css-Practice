d30d3001bf0872ccbd99d70b1681412b6c4d8fc5
SY <12tndbs12@naver.com>
2021-04-15 17:59:13 +0900

    레파지토리 생성

---
+++ baemin/js/index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+const download = document.querySelector(".btn_down_app");
+console.log(download);
+let hashName = location.hash
+
+const sec0 = document.querySelector("#section0");
+const sec1 = document.querySelector("#section1");
+const sec2 = document.querySelector("#section2");
+const sec3 = document.querySelector("#section3");
+const sec4 = document.querySelector("#section4");
+const sec5 = document.querySelector("#section5");
+const hi = sec0.className.split(" ");
+console.log(hi[2]);
+console.log(download);
+if (hi[2] == "active" && sec0.id == "section0") {
+
+}else{
+    
+}
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+HTML, CSS 연습용
+================
+# baemin
+* 배달의 민족 사이트 클론코딩
+# d2tex
+* d2tex사이트 클론코딩
+* <https://d2tex.co.kr/>
+# market
+* 마켓 컬리 메인 사이트 클론코딩
